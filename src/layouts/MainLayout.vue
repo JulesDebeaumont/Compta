e7de2295bf505b57deec77f0a66eece6f6f6ded7
Jules Debeaumont <deb.jules@outlook.fr>
2022-11-15 21:48:53 +0100

    first release, icons ok

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="bg-grey-10">
     <q-header elevated>
       <q-toolbar>
 
@@ -7,11 +7,31 @@
           La compta, c'est maintenant.
         </q-toolbar-title>
 
-        <q-btn>
+        <q-btn :to="{ name: 'about' }" @click="about = true">
           <q-item-section>
             <q-item-label>À propos</q-item-label>
           </q-item-section>
         </q-btn>
+
+        <q-dialog v-model="about">
+          <q-card class="bg-grey-10">
+            <q-card-section>
+              <div class="text-h6">À propos</div>
+            </q-card-section>
+
+            <q-card-section class="q-pt-none">
+              Créer par Jules Debeaumont OST™<br />
+              Dernière màj le 15/11/2022
+            </q-card-section>
+
+            <q-card-actions align="between">
+              <a target="_blank" href="https://github.com/JulesDebeaumont/Compta">
+                <q-btn flat label="GitHub" color="primary" />
+              </a>
+              <q-btn flat label="Fermer" color="primary" v-close-popup />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
 
       </q-toolbar>
     </q-header>
@@ -23,5 +43,9 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+
+// refs
+const about = ref(false)
 
 </script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// refs
+const defaultPrice = ref<number | null>(null)
+const priceWithTVA = ref<number | null>(null)
+const priceWithMarge = ref<number | null>(null)
+const priceWithMargeAndTVA = ref<number | null>(null)
+const priceClient = ref<number | null>(null)
+const marge = ref(30)
+const tva = ref(20)
+
+// functions
+function updatePrices(value: number | null) {
+  if (value != null) {
+    defaultPrice.value = value
+    priceWithTVA.value = Math.round((defaultPrice.value * (1 - (tva.value / 100))) * 100) / 100
+    priceWithMarge.value = Math.round((defaultPrice.value * (1 + (marge.value / 100))) * 100) / 100
+    priceWithMargeAndTVA.value = Math.round(((defaultPrice.value * (1 - (tva.value / 100))) * (1 + (marge.value / 100))) * 100) / 100
+    priceClient.value = Math.ceil(priceWithMargeAndTVA.value / 5) * 5
+  }
+}
+</script>
+
+<template>
+  <q-page class="column items-center justify-evenly">
+    <div class="q-gutter-y-md q-px-sm q-py-md column" style="min-width: 300px">
+      <q-input type="number" dark v-model="tva" color="secondary" label="TVA" @update:model-value="(value: number | null) =>
+      updatePrices(value)" suffix="%" />
+        <q-input type="number" dark v-model="marge" color="secondary" label="Marge"
+          @update:model-value="(value: number | null) => updatePrices(value)" suffix="%" class="q-pb-xl" />
+
+        <q-input type="number" dark v-model="defaultPrice" color="secondary" label="Prix"
+          @update:model-value="(value: number | null) => updatePrices(value)" suffix="€" class="q-pt-xl" />
+        <q-input disable dark v-model="priceWithTVA" color="secondary" label="Prix (TVA)" suffix="€" />
+        <q-input disable dark v-model="priceWithMarge" color="secondary" label="Prix avec marge" suffix="€" />
+        <q-input disable dark v-model="priceWithMargeAndTVA" color="secondary" label="Prix avec marge (TVA)" suffix="€"
+          class="q-pb-xl" />
+        <q-input disable dark v-model="priceClient" color="secondary" label="Prix client" suffix="€" class="q-pt-xl" />
+    </div>
+  </q-page>
+</template>
+
+<style>
+input[type="number"]::-webkit-outer-spin-button,
+  input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+</style>
